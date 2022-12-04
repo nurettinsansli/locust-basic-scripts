3b981408ee7e65df6dd402ecb9ec616b44e03ecd
Nurettin <45761126+nurettinsansli@users.noreply.github.com>
2022-12-04 18:53:57 +0300

    Create basic_locust_1.py

---
+++ basic_locust_1.py
@@ -0,0 +1,22 @@
+from locust import FastHttpUser,task
+
+url = 'https://api.demoblaze.com/login'
+
+payload = {
+    "username": "horacioss1",
+    "password": "1234567890"
+    }
+
+class MyUser(FastHttpUser):
+
+    @task()
+    def homePage(self):
+        self.client.get("/",name="HomePage")
+
+    @task()
+    def selectProduct(self):
+        self.client.get("/prod.html?idp_=1",name="Listing")
+
+    @task()
+    def login(self):
+        self.client.post(url, name="login",json=payload)
